3d8687abf875ece70dcdeab059f27c8c08ae8ef4
Vic Juan <vicjuan1003@gmail.com>
2017-07-23 01:39:38 -0600

    Add phone.php

---
+++ phone.php
@@ -0,0 +1,24 @@
+<?
+	include("connect.php");
+?>
+<html>
+	<head>
+		<title>好時光上課點名系統</title>
+		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+	</head>
+	<body bgcolor="#00BBBB">
+		<center>
+			<form method="POST" action="checkin.php">
+				Please select your name
+<?
+	$result = mysql_query("select * from student where number='$_POST[phone]'");
+	if(mysql_num_rows($result)){
+		while($row = mysql_fetch_array($result)){
+			echo "<input type=\"submit\" name=\"name\" value=\"".$row['name']."\">";
+		}
+	}
+?>
+			</form>
+		</center>
+	</body>
+</html>
