b238d2fbaefb48ffcbcd771e54810b4f68f76272
vicjuan <vicjuan1003@gmail.com>
2017-07-23 17:25:30 +0800

    Add checkin.php

---
+++ checkin.php
@@ -0,0 +1,35 @@
+<?
+	include("connect.php");
+	$result = mysql_query("insert into attend values ('$_POST[id]',date(current_time()))");
+	if(!$result){
+		die('點名失敗');
+	}
+?>
+<html>
+	<head>
+		<title>好時光上課點名系統</title>
+		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+	</head>
+	<body bgcolor="#00BBBB">
+		<center>
+			<?=$_POST[name]?>點名成功！
+			<table>
+				<tr>
+					<th>已上課日期</th>
+				</tr>
+				<?
+					$result = mysql_query("select * from attend where number='$_POST[id]'");
+					if(mysql_num_rows($result)){
+						while($row = mysql_fetch_array($result)){
+							echo "<tr>";
+							echo "<td>";
+							echo $row[date];
+							echo "</td>";
+							echo "</tr>";
+						}
+					}
+				?>
+			</table>
+		</center>
+	</body>
+</html>
--- phone.php
+++ phone.php
@@ -8,17 +8,18 @@
 	</head>
 	<body bgcolor="#00BBBB">
 		<center>
-			<form method="POST" action="checkin.php">
 				你是哪一位？<br>
 				<?
 					$result = mysql_query("select * from student where number='$_POST[phone]'");
 					if(mysql_num_rows($result)){
 						while($row = mysql_fetch_array($result)){
-							echo "<input type=\"submit\" name=\"name\" style=\"width:200px; height:75px; background-color:white; margin: 4px 2px; font-size:36;\" value=\"".$row['name']."\"><br>";
+							echo "<form method=\"POST\" action=\"checkin.php\">";
+							echo "<input type=\"hidden\" name=\"id\" value=\"".$row['id']."\">";
+							echo "<input type=\"submit\" style=\"width:200px; height:75px; background-color:white; margin: 4px 2px; font-size:36;\" value=\"".$row['name']."\"><br>";
+							echo "</form>";
 						}
 					}
 				?>
-			</form>
 		</center>
 	</body>
 </html>
