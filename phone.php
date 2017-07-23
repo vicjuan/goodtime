7542314b38956b91f89effb7a3e5b37508672a46
Vic Juan <vicjuan1003@gmail.com>
2017-07-23 02:53:37 -0600

    Add style

--- phone.php
+++ phone.php
@@ -14,7 +14,7 @@
 					$result = mysql_query("select * from student where number='$_POST[phone]'");
 					if(mysql_num_rows($result)){
 						while($row = mysql_fetch_array($result)){
-							echo "<input type=\"submit\" name=\"name\" style=\"width:200px\" value=\"".$row['name']."\"><br>";
+							echo "<input type=\"submit\" name=\"name\" style=\"width:200px; height:75px; background-color:white; margin: 4px 2px; font-size:36;\" value=\"".$row['name']."\"><br>";
 						}
 					}
 				?>
