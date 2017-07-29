093976fc853dcc44cc3b8733b309333788f4bf9f
vicjuan <vicjuan1003@gmail.com>
2017-07-29 15:08:10 +0800

    Add teacher.php

---
+++ teacher.php
@@ -0,0 +1,28 @@
+<?
+	include("connect.php");
+?>
+<html>
+	<head>
+		<title>好時光老師系統</title>
+		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+	</head>
+	<body bgcolor="#00BBBB">
+		<center>
+			<?
+\				echo "<table border=\"1\">";
+				echo "<tr><th>學生姓名</th></tr>";
+				$result = mysql_query("select * from student");
+				if(mysql_num_rows($result)){
+					while($row = mysql_fetch_array($result)){
+						echo "<tr>";
+						echo "<td>";
+						echo "<a href=\"student.php?id=". $row[id] ."\">" . $row[name] . "</a>";
+						echo "</td>";
+						echo "</tr>";
+					}
+				}
+				echo "</table>";
+			?>
+		</center>
+	</body>
+</html>
