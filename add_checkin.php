d6ce520f9d11a657aaa4cdb50c0adc0522b5e853
vicjuan <vicjuan1003@gmail.com>
2017-08-06 19:31:31 +0800

    Add add_checkin.php

---
+++ add_checkin.php
@@ -0,0 +1,21 @@
+<?
+	include("connect.php");
+?>
+<html>
+	<head>
+		<title>好時光上課點名系統</title>
+		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+	</head>
+	<body bgcolor="#00BBBB">
+		<center>
+			<?
+				$result = mysql_query("insert into attend (student_id, date) values ('$_POST[id]', '$_POST[date]'))");
+				if(!$result){
+					die('點名失敗' . mysql_error());
+				}
+				echo "新增點名成功！<br>";
+				echo "<a href=\"teacher.php\">回到老師首頁</a>";
+			?>
+		</center>
+	</body>
+</html>
