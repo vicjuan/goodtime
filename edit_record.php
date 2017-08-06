9ca235b62a8caf633f10ab96a66a6b4a2a8bceaf
vicjuan <vicjuan1003@gmail.com>
2017-08-06 18:45:28 +0800

    Add table

--- edit_record.php
+++ edit_record.php
@@ -15,8 +15,36 @@
 				$month = strtok("_");
 				$day = strtok("_");
 				$date = $year . "-" . $month . "-" . $day;
-				echo "select * from attend where student_id=$_GET[id] and unix_timestamp(time) >= unix_timestamp(\"$date\") and unix_timestamp(time) < unix_timestamp(\"$date\") + 86400";
-				$result = mysql_query("select * from attend where student_id=$_GET[id] and unix_timestamp(time) >= unix_timestamp(\"$date\") and unix_timestamp(time) < unix_timestamp(\"$date\") + 86400");
+				$result = mysql_query("select * from attend where student_id=$_GET[id] and unix_timestamp(date) >= unix_timestamp(\"$date\") and unix_timestamp(date) < unix_timestamp(\"$date\") + 86400");
+				if(mysql_num_rows($result)){
+					echo "<table cellpadding=\"0\" cellspacing=\"0\"  class=\"calendar\">";
+					echo "<tbody>";
+					echo "<tr class=\"calendar-row\">";
+					echo "<td class=\"calendar-day-head\">No.</th>";
+					echo "<td class=\"calendar-day-head\">上課日期</th>";
+					echo "<td class=\"calendar-day-head\"></th>";
+					echo "</tr>";
+					$index = 0;
+					while($row = mysql_fetch_array($result)){
+						$index++;
+						echo "<form method=\"POST\" action=\"record_edit_handle.php\">";
+						echo "<input type=\"hidden\" name=\"id\" value=\"" . $row[id] . "\">";
+						echo "<tr>";
+						echo "<td class=\"calendar-day\" style=\"width: initial;\">";
+						echo $index;
+						echo "</td>";
+						echo "<td class=\"calendar-day\" style=\"text-align: center;\">";
+						echo "<input type=\"text\" name=\"date\" value=\"" . $row[date] . "\">";
+						echo "</td>";
+						echo "<td class=\"calendar-day\" style=\"text-align: center;\">";
+						echo "<input type=\"submit\" value=\"送出\">";
+						echo "</td>";
+						echo "</tr>";
+					}
+					echo "</tbody>";
+					echo "</table>";
+					echo "</form>";
+				}
 			?>
 		</center>
 	</body>
