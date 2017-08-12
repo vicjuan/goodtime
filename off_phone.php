5ebd73472143f1f82ff8c791cd6941a656765f24
vicjuan <vicjuan1003@gmail.com>
2017-08-12 12:01:15 +0800

    Add off_phone.php

---
+++ off_phone.php
@@ -0,0 +1,26 @@
+<?
+	include("connect.php");
+?>
+<html>
+	<head>
+		<title>好時光請假系統</title>
+		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+	</head>
+	<body bgcolor="#00BBBB">
+		<center>
+				你是哪一位？<br>
+				<?
+					$result = mysql_query("select * from student where number='$_POST[phone]'");
+					if(mysql_num_rows($result)){
+						while($row = mysql_fetch_array($result)){
+							echo "<form method=\"POST\" action=\"off.php\">";
+							echo "<input type=\"hidden\" name=\"id\" value=\"".$row['id']."\">";
+							echo "<input type=\"hidden\" name=\"name\" value=\"".$row['name']."\">";
+							echo "<input type=\"submit\" style=\"width:200px; height:75px; background-color:white; margin: 4px 2px; font-size:36;\" value=\"".$row['name']."\"><br>";
+							echo "</form>";
+						}
+					}
+				?>
+		</center>
+	</body>
+</html>
