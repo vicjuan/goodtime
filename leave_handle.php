30bc3437108dc14d2ba9ef92d4d3b664a20811fe
vicjuan <vicjuan1003@gmail.com>
2017-08-12 17:35:41 +0800

    Add link

--- leave_handle.php
+++ leave_handle.php
@@ -11,14 +11,16 @@
 			<?
 				$result = mysql_query("select * from `leave` where student_id='$_POST[id]' and date='$_POST[date]'");
 				if(mysql_num_rows($result)){
-					echo "你已經在" . $_POST[date] . "這一天請假過了！";
+					echo "你已經在" . $_POST[date] . "這一天請假過了！<br>";
+					echo "<a href=\"index.html\">回到請假首頁</a>";
 				}
 				else {
 					$result = mysql_query("insert into `leave` (`student_id`, `date`) values ('$_POST[id]', '$_POST[date]')");
 					if(!$result){
 						die('請假失敗' . mysql_error());
 					}
-					echo "請假成功！";
+					echo "請假成功！<br>";
+					echo "<a href=\"index.html\">回到請假首頁</a>";
 				}
 			?>
 		</center>
