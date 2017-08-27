f2331a36fa2b1b9b57aad5a1a7485c0e0425f71a
vicjuan <vicjuan1003@gmail.com>
2017-08-27 12:57:41 +0800

    Fix selected option

--- lesson_edit.php
+++ lesson_edit.php
@@ -32,21 +32,57 @@
 						echo "</tr>";
 						echo "<tr>";
 						echo "<td class=\"calendar-day-np\" style=\"text-align: center;\">";
-						echo "<select name=\"day\" selected=\"". $_POST[day] ."\">";
-						echo "<option value=\"0\">星期天</option>";
-						echo "<option value=\"1\">星期一</option>";
-						echo "<option value=\"2\">星期二</option>";
-						echo "<option value=\"3\">星期三</option>";
-						echo "<option value=\"4\">星期四</option>";
-						echo "<option value=\"5\">星期五</option>";
-						echo "<option value=\"6\">星期六</option>";
+						echo "<select name=\"day\">";
+						for($index = 0; $index < 7; $index++){
+							echo "<option value=\"". $index . "\" ";
+							if($index == $row[day]){
+								echo "selected";
+							}
+							echo ">";
+							switch($index){
+								case 0:
+									echo "星期天";
+									break;
+								case 1:
+									echo "星期一";
+									break;
+								case 2:
+									echo "星期二";
+									break;
+								case 3:
+									echo "星期三";
+									break;
+								case 4:
+									echo "星期四";
+									break;
+								case 5:
+									echo "星期五";
+									break;
+								case 6:
+									echo "星期六";
+									break;
+							}
+							echo "</option>";
+						}
 						echo "</select> ";
 						echo "</td>";
 						echo "<td class=\"calendar-day-np\" style=\"text-align: center;\">";
 						echo "<select name=\"period\" selected=\"". $_POST[period] ."\">";
-						echo "<option value=\"MORNING\">早上</option>";
-						echo "<option value=\"AFTERNOON\">下午</option>";
-						echo "<option value=\"NIGHT\">晚上</option>";
+						echo "<option value=\"MORNING\" ";
+						if($row[period] == 'MORNING'){
+							echo "selected";
+						}
+						echo ">早上</option>";
+						echo "<option value=\"AFTERNOON\" ";
+						if($row[period] == 'AFTERNOON'){
+							echo "selected";
+						}
+						echo ">下午</option>";
+						echo "<option value=\"NIGHT\" ";
+						if($row[period] == 'NIGHT'){
+							echo "selected";
+						}
+						echo ">晚上</option>";
 						echo "</select> ";
 						echo "</td>";
 						echo "<td class=\"calendar-day-np\" style=\"text-align: center;\">";
