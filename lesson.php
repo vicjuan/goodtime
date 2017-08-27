b80cf3be121a0f9c1d8dcefdb58c20e79156af9d
vicjuan <vicjuan1003@gmail.com>
2017-08-27 11:39:06 +0800

    Add lesson.php

---
+++ lesson.php
@@ -0,0 +1,27 @@
+<?
+	include("connect.php");
+?>
+<html>
+	<head>
+		<title>好時光課程新增系統</title>
+		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+	</head>
+	<body bgcolor="#00BBBB">
+		<center>
+			<?
+				$result = mysql_query("select * from lesson where student_id='$_POST[id]' and day='$_POST[day]' and period='$_POST[period]'");
+				if(mysql_num_rows($result)){
+					echo "該時段已參加過了";
+				}
+				else{
+					$result = mysql_query("insert into lesson (student_id, day, period) values ('$_POST[id]', $_POST[day], $_POST[period]");
+					if(!$result){
+						die('新增失敗' . mysql_error());
+					}
+					echo $_POST[name]."新增成功！<br>";
+				}
+				echo "<a href=\"teacher.php\">回到老師首頁</a>";
+			?>
+		</center>
+	</body>
+</html>
