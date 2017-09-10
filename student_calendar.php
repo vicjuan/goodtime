a776e87116ad526834037daaba60ef52836b084f
vicjuan <vicjuan1003@gmail.com>
2017-09-10 21:34:20 +0800

    Add student_calendar.php

---
+++ student_calendar.php
@@ -0,0 +1,62 @@
+<?
+include("calendar.php");
+include("connect.php");
+
+function student_calendar ($name, $studentId) {
+	$calendars = [];
+	$ids = [];
+	$result = mysql_query("select unix_timestamp(date) as time from attend where student_id=$studentId order by time");
+	if(mysql_num_rows($result)){
+		while($row = mysql_fetch_array($result)){
+			$date = getdate($row[time]);
+			$year = $date[year];
+			$month = $date[mon];
+			$day = $date[mday];
+			$id = 'day_' . $year . '_' . $month . '_' . $day;
+			array_push($ids, $id);
+			if(!$calendars[$year]){
+				$calendars[$year] = [];
+			}
+			if(!$calendars[$year][$month]){
+				$calendars[$year][$month] = draw_calendar($month,$year);
+			}
+		}
+	}
+	foreach($calendars as $year){
+		foreach($year as $calendar){
+			echo $calendar;
+		}
+	}
+	$result = mysql_query("select sum(pay_class) as total from pay where student_id=$studentId");
+	$payTotal = mysql_fetch_array($result)[total];
+	$lastPay = 0;
+	if($payTotal > count($ids)){
+		$result = mysql_query("select pay_class from pay where student_id=$studentId order by time desc limit 1");
+		$lastPay = mysql_fetch_array($result)[pay_class];	
+	}
+?>
+	<script language="javascript">
+		var mark = function(id, className){
+			var element = document.getElementById(id);
+			element.className += " " + className;
+			var countDiv = element.getElementsByClassName("count-number")[0];
+			countDiv.innerText += "O ";
+		}
+<?
+	$counter = 0;
+	foreach($ids as $id){
+		$counter++;
+		$className = "pink";
+		if($counter <= $payTotal - $lastPay){
+			$className = "lightGreen";
+		}
+		else if($counter <= $payTotal){
+			$className = "lightBlue";
+		}
+		echo "mark(\"" . $id . "\", \"" . $className . "\");";
+	}
+?>
+	</script>
+<?
+}
+?>
