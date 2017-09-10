197edbae0427069522ea62d889931da9f9d3f800
vicjuan <vicjuan1003@gmail.com>
2017-09-10 11:22:50 +0800

    Apply template

--- leave_handle.php
+++ leave_handle.php
@@ -1,28 +1,61 @@
 <?
 	include("connect.php");
 ?>
-<html>
-	<head>
-		<title>好時光請假系統</title>
-		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
-	</head>
-	<body bgcolor="#00BBBB">
-		<center>
-			<?
-				$result = mysql_query("select * from `leave` where student_id='$_POST[id]' and date='$_POST[date]'");
-				if(mysql_num_rows($result)){
-					echo "你已經在" . $_POST[date] . "這一天請假過了！<br>";
-					echo "<a href=\"index.html\">回到請假首頁</a>";
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : Undeviating 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140322
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900|Quicksand:400,700|Questrial" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<h1><a href="#">好時光請假系統</a></h1>
+		</div>
+	</div>
+</div>
+<div class="wrapper">
+	<div id="welcome" class="container">
+		<div class="title">
+		<?
+			$result = mysql_query("select * from `leave` where student_id='$_POST[id]' and date='$_POST[date]'");
+			if(mysql_num_rows($result)){
+				echo "你已經在" . $_POST[date] . "這一天請假過了！";
+			}
+			else {
+				$result = mysql_query("insert into `leave` (`student_id`, `date`) values ('$_POST[id]', '$_POST[date]')");
+				if(!$result){
+					die('請假失敗' . mysql_error());
 				}
-				else {
-					$result = mysql_query("insert into `leave` (`student_id`, `date`) values ('$_POST[id]', '$_POST[date]')");
-					if(!$result){
-						die('請假失敗' . mysql_error());
-					}
-					echo "請假成功！<br>";
-					echo "<a href=\"index.html\">回到請假首頁</a>";
-				}
-			?>
-		</center>
-	</body>
+				echo "請假成功！<br>";
+			}
+		?>
+		</div>
+		<a href="index.html" class="button">回到請假首頁</a>
+	</div>
+</div>
+<div id="copyright">
+	<p>&copy; Untitled. All rights reserved. | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
+</body>
 </html>
