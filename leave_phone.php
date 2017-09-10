04ab88a0fd39d54e78cc01ed68b1c16a5d388809
vicjuan <vicjuan1003@gmail.com>
2017-09-10 10:36:49 +0800

    Apply template

--- leave_phone.php
+++ leave_phone.php
@@ -1,26 +1,60 @@
 <?
 	include("connect.php");
 ?>
-<html>
-	<head>
-		<title>好時光請假系統</title>
-		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
-	</head>
-	<body bgcolor="#00BBBB">
-		<center>
-				你是哪一位？<br>
-				<?
-					$result = mysql_query("select * from student where number='$_POST[phone]'");
-					if(mysql_num_rows($result)){
-						while($row = mysql_fetch_array($result)){
-							echo "<form method=\"POST\" action=\"leave.php\">";
-							echo "<input type=\"hidden\" name=\"id\" value=\"".$row['id']."\">";
-							echo "<input type=\"hidden\" name=\"name\" value=\"".$row['name']."\">";
-							echo "<input type=\"submit\" style=\"width:200px; height:75px; background-color:white; margin: 4px 2px; font-size:36;\" value=\"".$row['name']."\"><br>";
-							echo "</form>";
-						}
-					}
-				?>
-		</center>
-	</body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : Undeviating 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140322
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900|Quicksand:400,700|Questrial" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<h1><a href="#">好時光請假系統</a></h1>
+		</div>
+	</div>
+</div>
+<div class="wrapper">
+	<div id="welcome" class="container">
+		<div class="title">
+			<h2>你是哪一位？</h2>
+		</div>
+		<?
+			$result = mysql_query("select * from student where number='$_POST[phone]'");
+			if(mysql_num_rows($result)){
+				while($row = mysql_fetch_array($result)){
+					echo "<form method=\"POST\" action=\"leave.php\">";
+					echo "<input type=\"hidden\" name=\"id\" value=\"".$row['id']."\">";
+					echo "<input type=\"hidden\" name=\"name\" value=\"".$row['name']."\">";
+					echo "<input type=\"submit\" class=\"button\" style=\"width:200px; height:75px; background-color:white; margin: 4px 2px; font-size:36; border:0;\" value=\"".$row['name']."\"><br>";
+					echo "</form>";
+				}
+			}
+		?>
+	</div>
+</div>
+<div id="copyright">
+	<p>&copy; Untitled. All rights reserved. | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
+</body>
 </html>
