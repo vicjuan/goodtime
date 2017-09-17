2a3de750e7701659e642cc606bde8a082dc957ce
vicjuan <vicjuan1003@gmail.com>
2017-09-17 10:28:10 +0800

    Apply template

--- student.php
+++ student.php
@@ -1,19 +1,42 @@
 <?
 	include("student_calendar.php");
 ?>
-<html>
-	<head>
-		<title>好時光老師系統</title>
-		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
-	</head>
-	<body bgcolor="#00BBBB">
-		<center>
-			<?
-				echo "學生" . $_GET[name] . "的出席情形<br>";
-				student_calendar($_GET[name], $_GET[id]);
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
 
-					// 繳費
-					echo "<hr>學生" . $_GET[name] . "的繳費情形";
+Name       : Undeviating 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140322
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>好時光老師系統</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900|Quicksand:400,700|Questrial" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<h1><a href="#">好時光老師系統</a></h1>
+		</div>
+	</div>
+</div>
+<div class="wrapper">
+	<div id="three-column" class="container">
+		<div><span class="arrow-down"></span></div>
+		<div id="tbox1" style="width: 15%">
+			<div class="title">
+				<h2>學生<?=$_GET[name]?>的繳費情形</h2>
+<?
 					$result = mysql_query("select * from pay where student_id=$_GET[id] order by time desc");
 					if(mysql_num_rows($result)){
 						echo "<table cellpadding=\"0\" cellspacing=\"0\"  class=\"calendar\">";
@@ -46,9 +69,19 @@
 					echo "堂課的錢 ";
 					echo "<input type=\"submit\" value=\"繳費\" style=\"text-align:center; background:white; height: 30px; width: 50px; font-size: 16;\">";
 					echo "</form>";
-					
-					// 固定上課日
-					echo "<hr>學生" . $name . "的上課日期";
+?>
+			</div>
+		</div>
+		<div id="tbox2" style="width: 50%">
+			<div class="title">
+				<h2>學生<?=$_GET[name]?>的出席情形</h2>
+				<?=student_calendar($_GET[name], $_GET[id]);?>
+			</div>
+		</div>
+		<div id="tbox3" style="width: 15%">
+			<div class="title">
+				<h2>學生<?=$_GET[name]?>的上課日期</h2>
+<?
 					$result = mysql_query("select * from lesson where student_id=$_GET[id]");
 					if(mysql_num_rows($result)){
 						echo "<table cellpadding=\"0\" cellspacing=\"0\"  class=\"calendar\">";
@@ -133,20 +166,16 @@
 					echo "</select> ";
 					echo "<input type=\"submit\" value=\"新增\">";
 					echo "</form>";
-					
-					echo "<a href=\"teacher.php\">回到老師首頁</a>";
-			?>
-		</center>
-	</body>
-
-	<script language="javascript">
-		var addLink = function (element){
-			window.location.assign("record_edit.php?id=" + <?=$_GET[id]?> + "&date=" + element.id);
-		}
-		var elements = document.getElementsByClassName("calendar-day");
-		for(var i = 0; i < elements.length; i++){
-			elements[i].addEventListener("click", function(){addLink(this);}, false);
-		}
-	</script>
-
+?>
+			</div>
+		</div>
+	</div>
+	<div id="welcome" class="container" style="border-top: 1px solid rgba(0,0,0,0.2);">
+		<a href="teacher.php" class="button">回到老師首頁</a>
+	</div>
+</div>
+<div id="copyright">
+	<p>&copy; Untitled. All rights reserved. | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
+</body>
 </html>
