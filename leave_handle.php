f4f9e84752cb042343f86f4f8808d3639c951828
vicjuan <vicjuan1003@gmail.com>
2017-09-17 17:47:34 +0800

    Add if else condition

--- leave_handle.php
+++ leave_handle.php
@@ -53,7 +53,14 @@ Released   : 20140322
 				?>
 			</h2>
 		</div>
-		<a href="index.html" class="button">回到請假首頁</a>
+		<?
+			if($_POST[name]){
+				echo "<a href=\"student.php?name=" . $_POST[name] . "&id=" . $_POST[id] . "\" class=\"button\">回到學生首頁</a>";
+			}
+			else {
+				echo "<a href=\"index.html\" class=\"button\">回到請假首頁</a>";
+			}
+		?>
 	</div>
 </div>
 <div id="copyright">
