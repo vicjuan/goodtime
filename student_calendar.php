816824d66ffcee5609f166fe397533476db4a326
Vic Juan <vicjuan1003@gmail.com>
2017-09-17 03:15:52 -0600

    Fix SQL error

--- student_calendar.php
+++ student_calendar.php
@@ -24,7 +24,7 @@ function student_calendar ($name, $studentId) {
 		}
 	}
 	
-	$result = mysql_query("select * leave where student_id=$studentId order by date");
+	$result = mysql_query("select * from `leave` where student_id=$studentId order by date");
 	if(mysql_num_rows($result)){
 		while($row = mysql_fetch_array($result)){
 			$date = getdate($row[date]);
