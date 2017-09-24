dcd756a34749c2bdb13b1e392cb94a0306100f2d
vicjuan <vicjuan1003@gmail.com>
2017-09-24 10:21:43 +0800

    Add leave_edit_handle.php

---
+++ leave_edit_handle.php
@@ -0,0 +1,65 @@
+<?
+	include("connect.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : Undeviating 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140322
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>好時光請假修改系統</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900|Quicksand:400,700|Questrial" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<h1><a href="#">好時光請假修改系統</a></h1>
+		</div>
+	</div>
+</div>
+<div class="wrapper">
+	<div id="welcome" class="container">
+		<div class="title">
+			<h2>
+			<?
+				if($_POST['action'] == "delete"){
+					$result = mysql_query("delete from leave where id=$_POST[id]");
+					if(!$result){
+						die('刪除失敗' . mysql_error());
+					}
+				}
+				else{
+					$result = mysql_query("update leave set date=\"$_POST[date]\" where id=$_POST[id]");
+					if(!$result){
+						die('修改失敗' . mysql_error());
+					}
+				}
+				echo "修改成功！";
+			?>
+			</h2>
+		</div>
+		<a href="student.php?name=<?=$_POST[name]?>&id=<?=$_POST[student_id]?>" class="button">回到學生首頁</a>
+	</div>
+</div>
+<div id="copyright">
+	<p>&copy; Untitled. All rights reserved. | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
+</body>
+</html>
