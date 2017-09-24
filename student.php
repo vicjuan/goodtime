dc2c55de4fcd10a0c084fff0a9a5f52628d485e7
vicjuan <vicjuan1003@gmail.com>
2017-09-24 12:56:55 +0800

    Fix layout

--- student.php
+++ student.php
@@ -88,34 +88,109 @@ Released   : 20140322
 		<div id="tbox1" style="width: 15%">
 			<div class="title">
 				<h2>繳費情形</h2>
-<?
-					$result = mysql_query("select * from pay where student_id=$_GET[id] order by time desc");
-					if(mysql_num_rows($result)){
-						echo "<ul>";
-						while($row = mysql_fetch_array($result)){
-							echo "<li>";
-							echo "<p><h3>" . $row[time] . "</h3></p>";
-							echo "<form method=\"POST\" action=\"pay_edit.php\">";
-							echo $row[pay_class] . "堂 ";
-							echo "<input type=\"submit\" value=\"修改紀錄\">";
-							echo "<input type=\"hidden\" name=\"id\" value=\"" . $row[id] . "\">";
-							echo "</form>";
-							echo "</li>";
-						}
-						echo "</ul>";
-					}
-					echo "<br><br>";
-					echo "<form method=\"POST\" action=\"pay.php\">";
-					echo "<input type=\"hidden\" name=\"id\" value=\"" . $_GET[id] . "\"> ";
-					echo "我要繳 ";
-					echo "<input type=\"text\" name=\"classes\" style=\"width: 30px;\"> ";
-					echo "堂課的錢 ";
-					echo "<input type=\"submit\" value=\"繳費\" class=\"button\" style=\"border:0;\">";
-					echo "</form>";
-?>
 			</div>
+<?
+				$result = mysql_query("select * from pay where student_id=$_GET[id] order by time desc");
+				if(mysql_num_rows($result)){
+					echo "<ul>";
+					while($row = mysql_fetch_array($result)){
+						echo "<li>";
+						echo "<p><h3>" . $row[time] . "</h3></p>";
+						echo "<form method=\"POST\" action=\"pay_edit.php\">";
+						echo $row[pay_class] . "堂 ";
+						echo "<input type=\"submit\" value=\"修改紀錄\">";
+						echo "<input type=\"hidden\" name=\"id\" value=\"" . $row[id] . "\">";
+						echo "</form>";
+						echo "</li>";
+					}
+					echo "</ul>";
+				}
+				echo "<br><br>";
+				echo "<form method=\"POST\" action=\"pay.php\">";
+				echo "<input type=\"hidden\" name=\"id\" value=\"" . $_GET[id] . "\"> ";
+				echo "我要繳 ";
+				echo "<input type=\"text\" name=\"classes\" style=\"width: 30px;\"> ";
+				echo "堂課的錢 ";
+				echo "<input type=\"submit\" value=\"繳費\" class=\"button\" style=\"border:0;\">";
+				echo "</form>";
+?>
+			<div class="title">
+				<h2>上課日期</h2>
+			</div>
+<?
+				$result = mysql_query("select * from lesson where student_id=$_GET[id]");
+				if(mysql_num_rows($result)){
+					echo "<ul>";
+					while($row = mysql_fetch_array($result)){
+						echo "<li>";
+						echo "<p><h3>";
+						switch($row[day]){
+							case 0:
+								echo "星期天";
+								break;
+							case 1:
+								echo "星期一";
+								break;
+							case 2:
+								echo "星期二";
+								break;
+							case 3:
+								echo "星期三";
+								break;
+							case 4:
+								echo "星期四";
+								break;
+							case 5:
+								echo "星期五";
+								break;
+							case 6:
+								echo "星期六";
+								break;
+						}
+						switch($row[period]){
+							case 'MORNING':
+								echo "早上";
+								break;
+							case 'AFTERNOON':
+								echo "下午";
+								break;
+							case 'NIGHT':
+								echo "晚上";
+								break;
+						}
+						echo "</h3></p>";
+						echo "<form method=\"POST\" action=\"lesson_edit.php\">";
+						echo "<input type=\"hidden\" name=\"id\" value=\"" . $row[id] . "\">";
+						echo "<input type=\"submit\" name=\"mode\" value=\"edit\">";
+						echo "<input type=\"submit\" name=\"mode\" value=\"delete\">";
+						echo "</form>";
+						echo "</li>";
+					}
+					echo "</ul>";
+				}
+				echo "<br><br>";
+				echo "<form method=\"POST\" action=\"lesson.php\">";
+				echo "<input type=\"hidden\" name=\"id\" value=\"" . $_GET[id] . "\"> ";
+				echo "新增課程 ";
+				echo "<select name=\"day\">";
+				echo "<option value=\"0\">星期天</option>";
+				echo "<option value=\"1\">星期一</option>";
+				echo "<option value=\"2\">星期二</option>";
+				echo "<option value=\"3\">星期三</option>";
+				echo "<option value=\"4\">星期四</option>";
+				echo "<option value=\"5\">星期五</option>";
+				echo "<option value=\"6\">星期六</option>";
+				echo "</select> ";
+				echo "<select name=\"period\">";
+				echo "<option value=\"MORNING\">早上</option>";
+				echo "<option value=\"AFTERNOON\">下午</option>";
+				echo "<option value=\"NIGHT\">晚上</option>";
+				echo "</select> ";
+				echo "<input type=\"submit\" value=\"新增\" class=\"button\" style=\"border:0;\">";
+				echo "</form>";
+?>
 		</div>
-		<div id="tbox2" style="width: 50%">
+		<div id="tbox2" style="width: 60%">
 			<div class="title">
 				<h2>修改基本資料</h2>
 				<form method="POST" action="student_edit.php">
@@ -137,83 +212,6 @@ Released   : 20140322
 					</table>
 					<input type="submit" value="修改" class="button" style="border:0;" >
 				</form>
-			</div>
-		</div>
-		<div id="tbox3" style="width: 15%">
-			<div class="title">
-				<h2>上課日期</h2>
-<?
-					$result = mysql_query("select * from lesson where student_id=$_GET[id]");
-					if(mysql_num_rows($result)){
-						echo "<ul>";
-						while($row = mysql_fetch_array($result)){
-							echo "<li>";
-							echo "<p><h3>";
-							switch($row[day]){
-								case 0:
-									echo "星期天";
-									break;
-								case 1:
-									echo "星期一";
-									break;
-								case 2:
-									echo "星期二";
-									break;
-								case 3:
-									echo "星期三";
-									break;
-								case 4:
-									echo "星期四";
-									break;
-								case 5:
-									echo "星期五";
-									break;
-								case 6:
-									echo "星期六";
-									break;
-							}
-							switch($row[period]){
-								case 'MORNING':
-									echo "早上";
-									break;
-								case 'AFTERNOON':
-									echo "下午";
-									break;
-								case 'NIGHT':
-									echo "晚上";
-									break;
-							}
-							echo "</h3></p>";
-							echo "<form method=\"POST\" action=\"lesson_edit.php\">";
-							echo "<input type=\"hidden\" name=\"id\" value=\"" . $row[id] . "\">";
-							echo "<input type=\"submit\" name=\"mode\" value=\"edit\">";
-							echo "<input type=\"submit\" name=\"mode\" value=\"delete\">";
-							echo "</form>";
-							echo "</li>";
-						}
-						echo "</ul>";
-					}
-					echo "<br><br>";
-					echo "<form method=\"POST\" action=\"lesson.php\">";
-					echo "<input type=\"hidden\" name=\"id\" value=\"" . $_GET[id] . "\"> ";
-					echo "新增課程 ";
-					echo "<select name=\"day\">";
-					echo "<option value=\"0\">星期天</option>";
-					echo "<option value=\"1\">星期一</option>";
-					echo "<option value=\"2\">星期二</option>";
-					echo "<option value=\"3\">星期三</option>";
-					echo "<option value=\"4\">星期四</option>";
-					echo "<option value=\"5\">星期五</option>";
-					echo "<option value=\"6\">星期六</option>";
-					echo "</select> ";
-					echo "<select name=\"period\">";
-					echo "<option value=\"MORNING\">早上</option>";
-					echo "<option value=\"AFTERNOON\">下午</option>";
-					echo "<option value=\"NIGHT\">晚上</option>";
-					echo "</select> ";
-					echo "<input type=\"submit\" value=\"新增\" class=\"button\" style=\"border:0;\">";
-					echo "</form>";
-?>
 			</div>
 		</div>
 	</div>
